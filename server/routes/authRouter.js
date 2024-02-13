aa548b747af0dc6eda8eb5824ab9e5ecea62af98
saurabh.s.ed <saurabh.singh@edvanta.com>
2024-02-14 01:07:51 +0530

    add some changes

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -120,7 +120,6 @@ router.post("/loginwithotp", [logInWithOtp], async (req, res) => {
 });
 
 router.post("/register", [register], async (req, res) => {
-  console.log('hello world');
   let { name, email, role, phoneNumber, password, profession, city } = req.body;
   try {
     let user = await authControls.getUserByEmailOrPhoneNumber(
