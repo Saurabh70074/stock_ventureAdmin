6ad0d6a0d10103a68422c5d4a975ca8dc39d0123
saurabh.s.ed <saurabh.singh@edvanta.com>
2024-02-15 01:32:18 +0530

    add some files

--- server/routes/utilRouter.js
+++ server/routes/utilRouter.js
@@ -2,45 +2,31 @@ const express = require("express");
 const router = express.Router();
 const s3 = require('../utils/s3');
 const fs = require('fs');
-const multer = require('multer');
 
 const formControls = require("../controllers/formControls");
 const auth = require("../middlewares/auth");
 const responseHandler = require("../utils/responseHandler");
 const customExceptions = require("../responseModels/customExceptions");
 
-// Multer configuration for handling file uploads
-const upload = multer({ dest: 'uploads/' }); // Destination folder for temporarily storing uploaded files
-
 //#region Store Media
-router.post("/uploadmedia", upload.single('video'), async (req, res) => {
+router.post("/uploadmedia", async (req, res) => {
   try {
     console.log("This is upload");
-    
-    if (!req.file) {
-      throw new customExceptions.BadRequestException("No file uploaded");
+    let response = [];
+    for (let i = 0; i < req.files.length; i++) {
+      let data = req.files[i];
+      const s3Key = `uploadmedia/${data.filename}-${data.fieldname}`;
+      const s3Object = s3.putObject(
+        data.mimetype,
+        fs.createReadStream(data.path),
+        s3Key
+      );
+      let s3Url = `https://${process.env.S3BUCKETNAME}.s3.ap-south-1.amazonaws.com/${s3Key}`;
+      response[i] = s3Url
     }
-    
-    // Constructing S3 object key
-    const s3Key = `uploadmedia/${req.file.filename}`;
 
-    // Uploading file to S3
-    const params = {
-      Bucket: process.env.S3BUCKETNAME,
-      Key: s3Key,
-      Body: fs.createReadStream(req.file.path),
-      ContentType: 'video/mp4' // Set the content type accordingly
-    };
-
-    const s3Object = await s3.upload(params).promise(); // Using AWS SDK method to upload object to S3
-
-    // Generating S3 URL
-    const s3Url = s3Object.Location;
-
-    // Sending success response
-    return responseHandler.sendSuccess(res, { s3Url }, req);
+    return responseHandler.sendSuccess(res, response, req);
   } catch (error) {
-    // Sending error response
     return responseHandler.sendError(res, error, req);
   }
 });
